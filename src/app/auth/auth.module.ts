5a1d9b8f685e9d5e0c83e12f6116322c5aae4640
Den <saloed2206@gmail.com>
2026-03-15 17:26:01 +0100

    update angular from 16 to 17

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { I18nModule } from '@app/i18n';
+
+import { TranslateModule } from '@ngx-translate/core';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login.component';
 
